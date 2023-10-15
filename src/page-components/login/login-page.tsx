c4b1c2f3f339e22225d0920743820e53940902a0
riley-han <66325334+riley-han@users.noreply.github.com>
2023-10-14 21:54:37 -0600

    login page created

---
+++ src/page-components/login/login-page.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+import LoginForm from "./components/login-form"
+
+const LoginPage = () => {
+  return (
+    <div>
+      <div className="w-full text-center">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import LoginPage from "../page-components/login"
+
+const Login = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  )
+}
+
+export default Login
